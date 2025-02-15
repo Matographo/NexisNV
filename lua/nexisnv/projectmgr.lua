5342d42b0480c75908be5720e60a79d2902c2658
Leonard Ramminger <Matographo@gmail.com>
2025-02-15 04:46:41 +0100

    Fixing Session problem (just using the std code instead of trying my own)

--- lua/nexisnv/projectmgr.lua
+++ lua/nexisnv/projectmgr.lua
@@ -2,35 +2,36 @@ return {
 	'charludo/projectmgr.nvim',
 	lazy = false, -- important!
 	config = function()
-    local possession = _G.possession
+		require("projectmgr").setup()
+    -- local possession = _G.possession
 
-    require("projectmgr").setup({
-        autoload_project = true,
-        open_last_project = true,
-        show_project_icons = true,
-        telescope = true,
-        -- session_support = "possession",
-
-		--       hooks = {
-		-- on_project_opened = function(project)
-		--     local possession = require("possession")
-		--     local session_name = project.name -- Nutze den Projektnamen als Session-Namen
-		--     local sessions = possession.session.list()
+		--   require("projectmgr").setup({
+		--       autoload_project = true,
+		--       open_last_project = true,
+		--       show_project_icons = true,
+		--       telescope = true,
+		--       -- session_support = "possession",
 		--
-		--
-		--     vim.cmd("Neotree source=filesystem reveal=true" .. project.path)
-		--
-		--     if sessions[session_name] then
-		-- 	-- Falls eine Session existiert, lade sie
-		-- 	possession.session.load(session_name)
-		--     else
-		-- 	-- Falls keine Session existiert, erstelle eine neue und speichere sie
-		-- 	possession.session.save(session_name)
-		-- 	print("Neue Session für Projekt '" .. session_name .. "' erstellt und gespeichert.")
-		--     end
-		-- end,
-		--       },
-    })
+		-- --       hooks = {
+		-- -- on_project_opened = function(project)
+		-- --     local possession = require("possession")
+		-- --     local session_name = project.name -- Nutze den Projektnamen als Session-Namen
+		-- --     local sessions = possession.session.list()
+		-- --
+		-- --
+		-- --     vim.cmd("Neotree source=filesystem reveal=true" .. project.path)
+		-- --
+		-- --     if sessions[session_name] then
+		-- -- 	-- Falls eine Session existiert, lade sie
+		-- -- 	possession.session.load(session_name)
+		-- --     else
+		-- -- 	-- Falls keine Session existiert, erstelle eine neue und speichere sie
+		-- -- 	possession.session.save(session_name)
+		-- -- 	print("Neue Session für Projekt '" .. session_name .. "' erstellt und gespeichert.")
+		-- --     end
+		-- -- end,
+		-- --       },
+		--   })
 
 
 -- vim.api.nvim_create_autocmd({"VimLeavePre"}, {
