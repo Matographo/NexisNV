54c26033532f7fd37408dc4d06c937d5f0b1f099
Leonard Ramminger <Matographo@gmail.com>
2025-02-19 02:05:58 +0100

    Make a new Telescope strucure so it is nicer to interact with

--- lua/nexisnv/telescope.lua
+++ lua/nexisnv/telescope.lua
@@ -1,6 +1,4 @@
 return {
-
-
 	"nvim-telescope/telescope.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" }, -- Plenary ist eine Abhängigkeit für Telescope
 	cmd = "Telescope",                   -- Lazy-load: Nur laden, wenn `:Telescope` aufgerufen wird
@@ -14,6 +12,25 @@ return {
 		local actions = require("telescope.actions")
 		require("telescope").setup({
 			defaults = {
+				layout_strategy = "horizontal",
+				layout_config = {
+					width = 0.7,
+					height = 0.85,
+					prompt_position = "top",
+					preview_width = 0.6,
+					preview_cutoff = 10,
+				},
+				sorting_strategy = "ascending",
+				prompt_prefix = "⌕ ",
+				selection_caret = "󰅂  ",
+				pickers = {
+					find_files = {
+						theme = "dropdown", -- Dropdown für eine kompakte Ansicht
+					},
+					live_grep = {
+						theme = "ivy", -- Ivy für eine bessere Sucherfahrung
+					},
+				},
 				mappings = {
 					i = {
 						["<CR>"] = actions.select_default + actions.center, -- Direkt öffnen & zentrieren
